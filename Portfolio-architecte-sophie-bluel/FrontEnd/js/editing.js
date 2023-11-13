4004c60b280c054958e793a5866230f17a67c1a4
BuildTools <unconfigured@null.spigotmc.org>
2023-11-13 23:22:50 +0100

    Added Editing Mode: - editing.js - editing.css

---
+++ Portfolio-architecte-sophie-bluel/FrontEnd/js/editing.js
@@ -0,0 +1,19 @@
+function checkMode() {
+    if(sessionStorage.getItem("logged") === "1") {
+        document.querySelector(".edition").style.display = "flex";
+        document.querySelector(".edit").style.display = "flex";
+        document.querySelector(".spacer").style.flex = "1";
+        document.querySelector("#filters").style.display = "none";
+    } else {
+        document.querySelector(".spacer").style.flex = "0";
+    }
+}
+
+function closeEditing() {
+    sessionStorage.clear();
+    document.querySelector(".edition").style.display = "none";
+    document.querySelector(".edit").style.display = "none";
+    document.querySelector(".spacer").style.flex = "0";
+    document.querySelector("#filters").style.display = "flex";
+    loadFilters();
+}
