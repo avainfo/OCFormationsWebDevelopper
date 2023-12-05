a5c1cdece2c09282aa71a51eff5d2a6425cb573e
avainfo <avainfoconseil@gmail.com>
2023-12-05 21:40:22 +0100

    TODO: #007

--- Portfolio-architecte-sophie-bluel/FrontEnd/js/login.js
+++ Portfolio-architecte-sophie-bluel/FrontEnd/js/login.js
@@ -1,27 +1,55 @@
 async function login() {
-    const email = document.getElementById("email");
-    const password = document.getElementById("password");
-    if (email.value === "" || password.value === "") {
-        // TODO: #004
-        return;
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    if (email === "" || password === "") {
+        showDialog();
     } else {
-        const request = await fetch("http://localhost:5678/api/users/login", {
-            method: "POST",
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                "email": "sophie.bluel@test.tld",
-                "password": "S0phie"
-            })
-        });
-        const now = new Date();
-        const time = now.getTime();
-        const expireTime = time + 1000 * 3600 * 2;
-        now.setTime(expireTime);
-        document.cookie="token=" + (await request.json())["token"] + ";expires=" + now.toUTCString() +";path=/";
+        try {
+            const response = await fetch("http://localhost:5678/api/users/login", {
+                method: "POST",
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    email, // sophie.bluel@test.tld
+                    password // S0phie
+                })
+            });
+            if (response.ok) {
+                const {token} = await response.json();
+                setCookie(token, 2);
+                location.href = "../index.html";
+            } else {
+                showDialog();
+            }
+        } catch (error) {
+            console.error("Error during login:", error);
+            showDialog();
+        }
     }
-    location.href = "../index.html";
-    sessionStorage.setItem("logged", "1");
+}
+
+function showDialog() {
+    const anim = [
+        { opacity: "0" },
+        { opacity: "1" },
+    ];
+
+    const animTiming = {
+        duration: 150,
+        iterations: 1,
+        fill: "forwards"
+    };
+
+    document.getElementsByTagName("dialog")[0].style.display = "flex";
+    document.getElementsByTagName("dialog")[0].animate(anim, animTiming);
+}
+
+function setCookie(value, hours) {
+    const now = new Date();
+    const expireTime = now.getTime() + hours * 60 * 60 * 1000;
+    now.setTime(expireTime);
+    document.cookie = `token=${value};expires=${now.toUTCString()};path=/`;
 }
